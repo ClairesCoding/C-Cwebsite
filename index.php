9806d50fd468b5c142f16460be7e67be096dc10f
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-05-23 11:40:12 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+<!--IDK HOW MUCH OF THE HEADER TO PUT IN YET-->
+
+<!--INSIGHT SECTION-->
+    <section class="container-fluid text-left">
+    <div class="row">
+        <div class="col-md-8">
+            <img class="img-responsive" src="images/insight.jpg"> <!--ADD NEW SRC-->
+        </div>
+         <div class="col-md-4 insightbg text-center">
+            <h5 class= "text-left">INSIGHT</h5>
+             <div class="blocker"></div>
+             <p class="insight-small-title"> What does our company do?</p>
+            <p class="insight-text">Much of the curent funding and sponsorship available for Non For Profits (NFP) is becoming increasing competative and takes NFP’s away from their core purpose  using funds to manage marketing, fundraising, sponsorship, stakeholders, grants applications and events development and delivery.  This program enables the NFP to remain true to their core purpose and service delivery wjile exploring diverse strategies to ensure their relevance into the future without a massive internal investment.</p>
+            <button type="button" class="btn btn-default">Read More</button>
+        </div>
+    </div>
+</section>
+    
+    
+<!--LEARNING SECTION-->
+<div class="container learningbg">
+    <div class="row">
+
+  <h5 class= "text-center title">LEARNING</h5>
+      <div class="col-sm-6 text-center learningbg1 half">
+            
+                    <div class="coachingbox">
+                        <img class="img-responsive Cicon" alt="coaching icon" src="images/coachingicon.jpg"> <!--ADD NEW SRC-->
+                        <p><br> Increase your skills and knowledge with our coaching sessions</p>
+                        <button type="button" class="btn btn-primary">Coaching</button>
+                    </div>
+          </div>
+                
+         <div class="col-sm-6 text-center learningbg2 half">
+             
+                <div class="resourcesbox">
+                    <img class="img-responsive Ricon" alt="resource icon" src="images/resources.jpg"> <!--ADD NEW SRC-->
+                    <p>Educate yourself about training and management for NFPs</p>
+                    <button type="button" class="btn btn-primary">Resources</button>
+                </div>
+           
+        </div>
+        
+        </div>
+    </div>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
