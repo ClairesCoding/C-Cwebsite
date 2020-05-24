81570b99331f0df38fb5c7c874b103f63c54f92b
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-05-25 04:46:50 +0800

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!--FOOTER-->
+<footer class="footer">
+  <div class="container">
+    <div class="col-md-6 text-left">
+      <h6>CONNECT</h6>
+        <img class="icons" src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/facebook-icon.png" alt="“facebook”">
+        <img  src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/instagram-icon.png" alt="“instagram”">
+        <img  src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/twitter-icon.png" alt="“twitter”">
+    </div>
+    <div class="col-md-6 text-left">
+        <h7>CONTACT US</h7>
+    <p class="footer-right">
+          Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.<br>
+          Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.<br>
+          Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.<br>
+          Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.
+        </p>
+    </div>
+  </div>
+    <div class="col-md-6 text-left">
+        <img class="footer-image" src="images/logo.png">
+    </div>
+    <div class="col-md-6 text-center collapse navbar-collapse footer-small">
+<a href="#home" class="active">Home</a>
+<a href="#">Connecters and Partners</a>
+<a href="#">Get Involved</a>
+<a href="#">Learning</a>
+<a href="#">Insights</a>
+<a href="#">Connect</a>
+        
+    </div>
+
+    
+</footer>
+<?php wp_footer(); ?>
