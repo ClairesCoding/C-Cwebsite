b4c9aae0706196cea523a90ead71773c3287c598
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-05-29 15:16:12 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
   <div class="container">
     <div class="col-md-6 text-left">
       <h6>CONNECT</h6>
-        <img class="icons" alt="facebook" src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/facebook-icon.png">
-        <img  alt="instagram" src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/instagram-icon.png">
-        <img  alt="twitter"src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/twitter-icon.png">
+        <img class="icons" alt="facebook" src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/facebook-icon.png">
+        <img  alt="instagram" src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/instagram-icon.png">
+        <img  alt="twitter"src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/twitter-icon.png">
     </div>
     <div class="col-md-6 text-left">
         <h7>CONTACT US</h7>
@@ -18,7 +18,7 @@
     </div>
   </div>
     <div class="col-md-6 text-left">
-        <img class="footer-image" alt="logo" src="http://206.189.45.97/~mesh10/wp-content/themes/C-Cwebsite/images/logo.jpg">
+        <img class="footer-image" alt="logo" src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/logo.png">
     </div>
     <div class="col-md-6 text-center collapse navbar-collapse footer-small">
 <a href="#home" class="active">Home</a>
