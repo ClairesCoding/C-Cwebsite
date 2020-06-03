5fd92dfb36b420055f50dc39aa824072a6a03f0d
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-06-03 14:27:35 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,43 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+
+
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+<!--NEW SRC SERVER: http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/-->
+
+<!--INSIGHT SECTION-->
+    <section class="container-fluid text-left">
+    <div class="row">
+        <div class="col-md-8">
+            
+        </div>
+         <div class="col-md-4 insightbg text-center">
+            
+        </div>
+    </div>
+</section>
+    
+    
+<!--LEARNING SECTION-->
+<div class="container learningbg">
+
+    <div class="row">
+
+  <h5>LEARNING</h5>
+      <div class="col-sm-6 text-center learningbg1 half">
+            
+
+          </div>
+                
+         <div class="col-sm-6 text-center learningbg2 half">
+
+           
+        </div>
+        
+        </div>
+    </div>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
