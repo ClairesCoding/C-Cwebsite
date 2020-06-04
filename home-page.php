a63f2958798fa3ebb535d568aa096636f528aded
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-06-04 12:14:48 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -26,14 +26,19 @@
 <div class="container learningbg">
 
     <div class="row">
-
-  <h5>LEARNING</h5>
+  <h5 class= "text-center title"><?php the_field('sectionheader'); ?></h5>
+        
       <div class="col-sm-6 text-center learningbg1 half">
-            
+          <img class="img-responsive Cicon" alt=“coachingicon” src="<?php the_field('displayimage'); ?>">
+            <p><?php the_field('explanationtext'); ?> </p>
+            <a href="<?php the_field('calltoactionbutton'); ?>" class="btn btn-primary">Resources</a>
 
           </div>
                 
          <div class="col-sm-6 text-center learningbg2 half">
+             <img class="img-responsive Ricon" alt=“Resourcesicon” src="<?php the_field('displayimage'); ?>">
+            <p><?php the_field('explanationtext'); ?> </p>
+            <a href="<?php the_field('calltoactionbutton'); ?>" class="btn btn-primary">Coaching</a>
 
            
         </div>
