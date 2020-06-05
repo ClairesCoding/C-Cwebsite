e7335685f34dcbbb864e9f4648df4cdd21427a6f
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-06-05 21:48:46 +0800

    Create base.php

---
+++ base.php
@@ -0,0 +1,18 @@
+<?php
+    /*
+    Template Name: Base 
+    */
+    ?>
+
+
+<?php include (TEMPLATEPATH . '/small-header.php');  /* Tells WordPress to include the smaller header header.php */ ?>
+<section>
+
+
+
+
+
+
+
+</section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
