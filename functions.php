c44bc9f13a37bdcc962c3c53ca2db584cdc1c26c
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-06-05 11:29:36 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,9 +22,7 @@ if($args->list_item_class){$classes[] = $args->list_item_class;
                           }return $classes;
     }
 add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
-?>
-<?php
-?>
+ ?>
 <?php
 	if ( get_field(' headerbackgroundimage') ) {
 		echo 'style="background: url(' . get_field(' headerbackgroundimage') . ')"';
