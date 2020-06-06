80a009a2a622b2d281391a3f765f0c53a5ffc658
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-06-06 20:15:13 +0800

    Update base.php

--- base.php
+++ base.php
@@ -6,17 +6,38 @@
 
 
 <?php include (TEMPLATEPATH . '/small-header.php');  /* Tells WordPress to include the smaller header header.php */ ?>
-<section>
+<div class="container-fluid maincontainer">
+    <div class="container">
+        <div class="row">
+            <section class="col-sm-7 maintext">				
+    <article class="insight-text"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
+	
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+<div class="post single-page">
+		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
 
+		<div class="postcontent">
+			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
+			<?php the_content(); ?>
+		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
+        </div><!--postcontet-->
+		
+	</div><!--post class-->
 
-
-<?php $post_id = 94;
-$queried_post = get_post($post_id);?>
-<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-<h4><?php echo $queried_post->post_title; ?></h4>
-<?php echo $queried_post->post_excerpt; ?>
-
-
-
+   
+<?php endwhile; ?>
+	
+	<?php else: ?>
+	<div class="post">
+		<p>Sorry, no events found.</p>
+	</div><!--post-->
+	<?php endif; ?>
+	</article><!---->
 </section>
+            
+     </div>
+    </div>
+  </div>
+
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
