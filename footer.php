6b517a83a5478972fb0a9a0755f873d410e96cec
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-06-10 17:20:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,34 +2,39 @@
 <footer class="footer">
   <div class="container">
     <div class="col-md-6 text-left">
-      <h6>CONNECT</h6><br>
-        <img href="#" class="icons" alt="facebook" src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/facebook-icon.png">
-        <img  href="#" alt="instagram" src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/instagram-icon.png">
-        <img  href="#" alt="twitter"src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/twitter-icon.png">
+        <h6>LINKS</h6><br>
+          <a href="file:///C:/Users/tomas/Desktop/Website/C-Cwebsite/index.html" class="active footerlinks">BLOG AND ARTICLES</a><br>
+          <a href="#" class="active footerlinks">PODCAST</a><br>
+          <a href="#" class="active footerlinks">EDUCATION</a><br>
+        <a href="#" class="active footerlinks">PRIVACY POLICY</a><br>
+          <a href="#" class="active footerlinks">EVENTS</a><br><br>
+
     </div>
     <div class="col-md-6 text-left">
-        <h7>CONTACT US</h7>
+        <h6>CONNECT WITH US</h6>
     <p class="footer-right">
         <br>
-        Email us at <a href="mailto:someone@example.com">collabcollective@humanitashr.com.au</a>
-        <br>
-        <br>
-        <br>
+     <a href="mailto:someone@example.com" class="active footerlinks">collabcollective@humanitashr.com.au</a><br>
+        <p class="linkedinname"><a href="https://au.linkedin.com/in/biancastarcevich" class="active footerlinks"><img class="icons" alt="LinkedIn" src="images/linkedin.png"></a>Bianca Starcevich</p>
+        <p class="linkedinname"><a href="https://au.linkedin.com/in/sue-pember-6b969b20" class="active footerlinks"><img class="icons" alt="LinkedIn" src="images/linkedin.png"></a>Sue Pember</p><br>
+        
 
-        </p>
+        
     </div>
   </div>
     <div class="col-md-6 text-left">
-        <img class="footer-image" alt="logo" src="http://www.collabcollective.com.au/wp-content/themes/C-Cwebsite/images/logo.jpg">
+        <img class="footer-image" alt="logo" src="/images/logo.jpg">
     </div>
     <div class="col-md-6 text-center collapse navbar-collapse footer-small">
 <a href="#home" class="active">Home</a>
-<a href="#">Partners</a>
 <a href="#">Get Involved</a>
-<a href="#">Learning</a>
 <a href="#">Insights</a>
+<a href="#">Learning</a>
 <a href="#">Connect</a>
         
-    </div>   
+    </div>
+
+    
 </footer>
+
 <?php wp_footer(); ?>
