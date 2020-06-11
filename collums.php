6f942c3156e6335cf00313ed7904f005846958b8
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-06-11 15:19:14 +0800

    Update collums.php

--- collums.php
+++ collums.php
@@ -15,7 +15,7 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 <div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
+		
 
 		<div class="postcontent">
 			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
