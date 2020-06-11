4d41f5f2278b5ff6b1a2409554dc7be791bbf7d3
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-06-12 04:51:41 +0800

    Update insight-template1.php

--- insight-template1.php
+++ insight-template1.php
@@ -15,6 +15,7 @@
         <div class="blocker2"></div>
         <p class="insight-small-title2">What does our company do?</p>
         <div class="row about-text">
+
         <p class="director-small">Collab Collective is a learning Social Enterprise / Movement that  was started with the core purpose of connecting the dots to enable shared positive outcomes including productivity and financial for organisations.
 Collab Collective is a powerful network to help facilitate change along with critical and diverse thinking within your organisation. Through an exploration of new ideas, we link industry and not-for-profits to change the world and make a difference. IIt is important that not-for-profits are able to engage with their current community so through purpose and outcomes driven support, Collab Collective offers services which strive for shared knowledge to enable growth, current skill enhancement, and critical thinking through experience. 
 <br>
@@ -30,15 +31,16 @@ Collab Collective assists not-for-profits, through a link with industry, as they
  <section class="container fluid insight-buttons">
         <div class="col-sm-6 text-center connectorsbg">
             <div class="connectorsbox">
+
                 <p class="insight-small-title2"><br>Tune in to our podcast and find out more information.<br><br></p>
-        <a href="<?php the_field('calltoactionbutton'); ?>" class="btn btn-primary">Podcasts</a>
+        <a href="http://www.collabcollective.com.au/podcast-2/" class="btn btn-primary">Podcasts</a>
         </div>
         </div>
         
         <div class="col-sm-6 text-center connectorsbg">
         <div class="involvedbox">
             <p class="insight-small-title2">Read our blog for updated information and articles.<br><br></p>
-        <a href="<?php the_field('calltoactionbutton'); ?>" class="btn btn-primary">Blog</a>
+        <a href="http://www.collabcollective.com.au/insight__trashed/blog/" class="btn btn-primary">Blog</a>
         </div>
         </div>
     </section>
@@ -49,11 +51,11 @@ Collab Collective assists not-for-profits, through a link with industry, as they
 <section class="container-fluid directorsbg1">
     <div class="container">
   <div class="directors-text">
-      <h4>DIRECTORS</h4>
+
+<h4>DIRECTORS</h4>
     <p class="director-small1">We are a female founded organisation aiming to make the world a better place. With the perfect balance between managing large organisations and innovative entrepreneurship, Sue and Bianca bring you Collab Collective linking industry and not-for-profits to explore new ideas and the power of connection. </p>
       <img class="img-responsive directorimage" alt="director image" src="<?php the_field('directorsimage'); ?>"> 
-          
-        </div>
+</div>
 <!--
     </div>
            <div class="whitebox1">
@@ -61,17 +63,18 @@ Collab Collective assists not-for-profits, through a link with industry, as they
         <div class="col-md-12" style="background-color: white;padding-top:20px;margin-left:auto;margin-right:auto;margin-bottom:10%;">
 
             <div class="col-md-6">
-                <p class="insight-small-title2">BIANCA STARCEVICH</p>
+
+<p class="insight-small-title2">BIANCA STARCEVICH</p>
             <!--            <div class="col-md-6">-->
         <p class="director-small2">Bianca Starcevich is a HR executive with over 20 years of experience across a range of industries. Her early career began in HR roles with Bankwest, Rio Tinto, Iluka and Wesfarmers. At Bunnings she had a number of roles, with her favourite role sitting as part of the executive team and responsible for the delivery of innovative strategies, programs and policies to support employment, development and safety of 40,000 team across Australia and New Zealand. She started Humanitas HR in 2019 which was founded with a single mission; to help individuals, teams and businesses to be at their best. Her company purpose reflects her own personal values as like Sue, Bianca realises the importance of giving back and contributing to the community. She has held many volunteer roles within the community with her current roles including Non Executive Director with 100 Women, Board Member at HLTF and Vice President for EFJCC.  Bianca is extremely passionate, dedicated and innovative in her thinking and care for others around her.</p>
+        <!--            <div class="col-md-6">-->
 <!--                </div>-->
         </div>  
         
 <!--        <div class="whitebox2a">-->
 <!--               <div style="background-color: white;padding-top:20px;margin-left:auto;margin-right:auto;margin-bottom:10%;">-->
-            <div class="col-md-6">
-            <p class="insight-small-title2">SUE PEMBER</p>
-                
+    <div class="col-md-6">
+        <p class="insight-small-title2">SUE PEMBER</p>               
 <!--            <div class="col-md-6">-->
         <p class="director-small3">Sue Pember has extensive experience in a number of areas particularly relating to small business, entrepreneurship and not-for-profit advocacy. Owning and directing two small businesses, Sue founded AussieOS Mobility Services in 2010 and Mi Casa Property Boutique in 2015 giving her an incredibly strong understanding of the world of start-ups. Aussie, specialising in personal relocation programs as workers and families transition to Australia, and Mi Casa, a boutique residential sales agency, truly value the community and personal relationships between members within this space. Her own personal value of advocating for a strong sense of community has seen her also take on many volunteer endeavours such as 100 Women where she currently performs as “Board Advisory – Strategy and Social Enterprise”, and an advocate for Think Perth, a campaign focusing on communicating investment, education opportunities and dynamic multicultural community in WA. Sue has recently launched her freelance business consultancy support services as well as being selected as a 40 under 40 judge for 2020 after her “People’s Choice” Award in the 2013 event. Sue is a dynamic, multi-faceted entrepreneur with an eye for opportunity and believes everyone deserves the chance to be involved and feel welcomed within a community or network of supportive people.</p>
 <!--                </div>-->
