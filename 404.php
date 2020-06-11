2267e118dbdc50ded3de3dad7f871410249d3731
ninatomas <61775981+ninatomas@users.noreply.github.com>
2020-06-11 16:49:06 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -6,20 +6,19 @@
 		<div id="content" class="site-content" role="main">
 
 			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
 			</header>
 
 			<div class="page-wrapper row">
 				
                 <div class="col-md-3"></div>
                 
-                <div class="col-md-6 page-content">
+                <div class="col-md-6 page-content searchsubmit searchform">
 					<h4><?php _e( 'Oops something went missing on our end', 'twentythirteen' ); ?></h4>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
-
-					<?php get_search_form(); ?>
+					<p><?php _e( 'It looks like nothing was found at this location. <br>We suggest going back to the home page.', 'twentythirteen' ); ?></p>
+                    <a href="http://www.collabcollective.com.au/" class="btn btn-default">Home Page</a>
 				</div><!-- .page-content -->
-                <div class="col-md-3 screen-reader-text"></div>
+                <div class="col-md-3 screen-reader-text">
+                    </div>
 			</div><!-- .page-wrapper -->
 
 		</div><!-- #content -->
